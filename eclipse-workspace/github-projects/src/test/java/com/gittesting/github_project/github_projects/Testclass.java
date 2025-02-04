5bce440d336f7a6feab83947203d70bf81b95259
neeraj.tiwari <neeraj.tiwari@CENT-NOI-L0217.Centilytics.local>
2025-02-04 20:35:08 +0530

    This is a testing commit

---
+++ eclipse-workspace/github-projects/src/test/java/com/gittesting/github_project/github_projects/Testclass.java
@@ -0,0 +1,10 @@
+package com.gittesting.github_project.github_projects;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		System.out.println("This is for testing purpose");
+
+	}
+
+}
