8445667aa62bfcdf6fe2a3d9d9b8d35f9261db26
neeraj.tiwari <neeraj.tiwari@CENT-NOI-L0217.Centilytics.local>
2025-02-12 14:28:59 +0530

    This is first commit

---
+++ eclipse-workspace/Jenkinsintegrate/src/Jenkinsinteg/Jenkinstest.java
@@ -0,0 +1,35 @@
+package Jenkinsinteg;
+
+import org.testng.annotations.Test;
+
+public class Jenkinstest {
+
+	@Test
+	public void testcase1()
+	{
+		
+        System.out.println("This is testcase1");
+	}
+	
+	@Test
+	public void testcase2()
+	{
+		
+        System.out.println("This is testcase2");
+	}
+	
+	@Test
+	public void testcase3()
+	{
+		
+        System.out.println("This is testcase3");
+	}
+	
+	@Test
+	public void testcase4()
+	{
+		
+        System.out.println("This is testcase4");
+	}
+
+}
---
+++ eclipse-workspace/Jenkinsintegrate/src/Jenkinsinteg/Jenkinstesting.java
@@ -0,0 +1,36 @@
+package Jenkinsinteg;
+
+import org.testng.annotations.Test;
+
+public class Jenkinstesting {
+	
+
+	@Test
+	public void testcase1()
+	{
+		
+        System.out.println("This is testcase1");
+	}
+	
+	@Test
+	public void testcase2()
+	{
+		
+        System.out.println("This is testcase2");
+	}
+	
+	@Test
+	public void testcase3()
+	{
+		
+        System.out.println("This is testcase3");
+	}
+	
+	@Test
+	public void testcase4()
+	{
+		
+        System.out.println("This is testcase4");
+	}
+
+}
